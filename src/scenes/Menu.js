bd7165efe5984e1fa0b9d345b2fd0a0c329014c6
Rinnywheats <cirnoanon@gmail.com>
2021-04-11 16:01:23 -0700

    functional for monday baybeeee

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,9 +4,19 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
-
-    create() {
-        this.add.text(20,20, "Rocket Patrol Menu");
-        this.scene.start("playScene");
+    preload() {
+        // load audio
+        this.load.audio('sfx_explosion', './assets/explosionsfx.wav');
+      }
+     create() {
+        this.add.text(210,20, "Rocket Patrol Main Menu");
+        this.add.text(120,220, "Use the arrow keys to move, and F to fire!");
+        this.add.text(250,250, "Shoot to start!");
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+    }
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.scene.start('playScene');
+         }
     }
 }
