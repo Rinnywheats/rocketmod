ab60149a34fe58ce1a222eea273898a8d176833f
Rinnywheats <cirnoanon@gmail.com>
2021-04-16 15:21:43 -0700

    proper commit with comments

--- src/main.js
+++ src/main.js
@@ -11,3 +11,14 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
 let keyLEFT, keyRIGHT, keyF, keyR;
+
+//My name is Ashley Chapp! This is Rocket Patrol Mod, 4/16!
+// This took me roughly ten hours.
+//POINTS:
+//60: Redesigned art, UI, and sound
+//10: Parralaxing!
+//10: animated shipe
+//20: new ship! Batty-chan!
+//dragon and sfx Oracle of Seasons, nintendo
+//bat, Castlevania, Konami
+//player sprite: myself
